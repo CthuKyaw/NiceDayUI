1dde85cc3c54f49ef0d596777c3a5a17cb91c539
Dolphin <cthukyaw13@gmail.com>
2021-10-29 00:03:10 +0630

    Test

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -4,7 +4,7 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io(`${process.env.REACT_APP_API_HOST}/api`);
+const socket = io(`${process.env.REACT_APP_API_HOST}`);
 //const socket = io('http://localhost:3000');
 //const socket = io('https://warm-wildwood-81069.herokuapp.com');
 
